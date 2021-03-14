9c25d571596f288c50ec0a146f4f837b4567f30f
zlopez101 <zachlopez9@gmail.com>
2021-03-14 16:00:47 -0500

    pytest account successful calls complete, still need to do fails

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,16 +1,45 @@
 import pytest
 import requests
+import PyTradier
 from PyTradier.account import Account
+import json
 
 
-def test_account_profile(monkeypatch):
-    """
-    GIVEN PyTradier API's access to the account module
-    WHEN the account module doesn't require parameter input
-    THEN appropriate return responses need to be retrieved
-    """
+class TestAccount:
 
-    # account
     account = Account()
-    profile = account.profile()
-    print(profile.json())
+
+    def test_profile(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\profile.json")
+        profile = self.account.profile()
+        assert patch.mocked.response == profile.json()
+
+    def test_balances(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\balances.json")
+        balances = self.account.balances()
+        assert patch.mocked.response == balances.json()
+
+    def test_positions(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\positions.json")
+        positions = self.account.positions()
+        assert patch.mocked.response == positions.json()
+
+    def test_orders(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\orders.json")
+        orders = self.account.orders()
+        assert patch.mocked.response == orders.json()
+
+    def test_history(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\history.json")
+        history = self.account.history()
+        assert patch.mocked.response == history.json()
+
+    def test_gainloss(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\gainloss.json")
+        gainloss = self.account.gainloss()
+        assert patch.mocked.response == gainloss.json()
+
+    def test_order(self, patch_get):
+        patch = patch_get(200, r"tests\Success_API_responses\account\order.json")
+        order = self.account.order("OrderId")
+        assert patch.mocked.response == order.json()
